5bc90d65ab7e125b6283cb99fe800bf4163fb721
Bryan Morfe <bryanmorfe@gmail.com>
2019-10-16 17:40:40 -0400

    Initial commit

---
+++ source/scomm.c
@@ -0,0 +1,19 @@
+#include "scomm.h"
+
+#include <stdint.h>
+#include <unistd.h>
+
+struct scomm_config_s
+{
+    uint8_t wait_time;
+};
+
+void sendByte( byte_t byte )
+{
+    
+}
+
+byte_t receiveByte( void )
+{
+    
+}
---
+++ source/scomm.h
@@ -0,0 +1,14 @@
+#ifndef _SCOMM_H
+#define _SCOMM_H
+
+#include <stdint.h>
+
+void Serial_CommInit( void );
+
+void Serial_CommDeInit( void );
+
+void Serial_SendByte( uint8_t byte );
+
+uint8_t Serial_ReceiveByte( void );
+
+#endif /* _SCOMM_H */
